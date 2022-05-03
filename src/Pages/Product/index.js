fdab0b1c1dd456c56e3008d6d6e94781a8beff4f
thiagovss <tvictorss03@gmail.com>
2022-05-02 21:04:01 -0300

    commit 2

--- src/Pages/Product/index.js
+++ src/Pages/Product/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Product(props){
     return(
         <div>
-        <img src={props.image} alt='imagem do produto'/>
+        <img src={props.image} alt='qqr coisa'/>
         <h1>{props.titulo}</h1>
         </div>
     )
